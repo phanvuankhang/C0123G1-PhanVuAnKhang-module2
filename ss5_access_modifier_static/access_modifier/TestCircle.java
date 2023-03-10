16eecd4358ca7895d957236539e8a2cce572705a
phanvuankhang <phanvuankhang@gmail,com>
2023-03-10 15:31:37 +0700

    firt commit

---
+++ ss5_access_modifier_static/access_modifier/TestCircle.java
@@ -0,0 +1,15 @@
+package access_modifier;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập bán kính");
+        double radius = sc.nextDouble();
+        Circle c1 = new Circle(radius);
+        System.out.println("Diện tích hình tròn là : " + c1.getArea());
+    }
+
+}
